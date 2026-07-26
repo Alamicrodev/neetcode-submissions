f230854361188c15f2301afc2667db138bb33bc3
Mohid Alam <mohidalamsohail22@live.com>
2026-07-26 11:21:05 +0500

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.ts
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+
+    isPalindrome(s: string): boolean {
+
+       let i = 0; 
+       let j = s.length-1; 
+       
+       function isAlphaNumeric(c: string): boolean {
+              
+              let code = c.charCodeAt(0); 
+
+              return  ((code >= 48 && code <= 57) || (code >= 65 && code <= 90) || (code >= 93 && code <= 122))
+
+       }
+    
+
+       while ( i<s.length && j >= 0 ) {
+          
+          if (!isAlphaNumeric(s[i])) {
+             i++; 
+             continue; 
+          }
+
+          if (!isAlphaNumeric(s[j])) {
+            j--; 
+            continue;
+          }
+
+          if (s[i].toLowerCase() == s[j].toLowerCase()) {
+              i++;
+              j--; 
+              continue; 
+          }
+          else {
+            return false 
+          }
+       }
+       
+       return true; 
+
+    }
+}
