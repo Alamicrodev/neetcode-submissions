73df4172a198dc2f71d7b34cc7909ff7198e8034
Mohid Alam <mohidalamsohail22@live.com>
2026-08-05 08:24:37 +0500

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.ts
@@ -0,0 +1,41 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s: string, k: number): number {
+
+        let i = 0; 
+        let j = 0; 
+
+        let charHash = {}
+        let maxCharCount = 1; 
+        let res = 0; 
+         
+
+        while (j < s.length) { 
+             if (charHash[s[j]]) {
+                charHash[s[j]]++; 
+                if (charHash[s[j]] > maxCharCount) {
+                    maxCharCount = charHash[s[j]]
+                }
+               }
+               else {
+                   charHash[s[j]] = 1; 
+               }
+            if ((j-i+1)-maxCharCount > k) {
+                 charHash[s[i]]--; 
+                i++;
+                
+            }
+            if (res < (j-i+1)) {
+                    res = j-i+1; 
+                } 
+               j++;
+
+        } 
+         
+        return res
+    }
+}
